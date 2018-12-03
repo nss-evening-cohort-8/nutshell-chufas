10ead3961ff13a892ff51de40ce213af3c543e47
feven mulugeta <fevenmulugeta@fevens-MacBook-Pro.local>
2018-12-02 20:04:38 -0600

    articles printed to the page

--- src/helpers/authHelpers.js
+++ src/helpers/authHelpers.js
@@ -1,8 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import $ from 'jquery';
+// import allArticles from '../components/NewsArticles/GetArticles/articles';
 
-const checkLoginStatus = () => {
+const checkLoginStatus = (initializeArticlesPage) => {
   firebase.auth().onAuthStateChanged((user) => {
     if (user) {
       $('#auth').hide();
@@ -10,13 +11,17 @@ const checkLoginStatus = () => {
       $('#navbar-button-auth').hide();
       $('#navbar-button-new').show();
       $('#navbar-button-logout').show();
+      $('#articles').show();
+      initializeArticlesPage();
     } else {
       $('#completed-tasks').hide();
       $('#navbar-button-auth').show();
       $('#navbar-button-new').hide();
       $('#navbar-button-logout').hide();
+      $('#articles').hide();
     }
   });
 };
+const getCurrentUid = () => firebase.auth().currentUser.uid;
 
-export default { checkLoginStatus };
+export default { checkLoginStatus, getCurrentUid };
--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ import apiKeys from '../db/apiKeys.json';
 import auth from './components/Auth/auth';
 import navbar from './components/Navbar/navbar';
 import authHelpers from './helpers/authHelpers';
+import getArticles from './components/NewsArticles/GetArticles/articles';
 
 import './index.scss';
 
 const initApp = () => {
   firebase.initializeApp(apiKeys.firebaseKeys);
   navbar.createNavbar();
-  authHelpers.checkLoginStatus();
+  authHelpers.checkLoginStatus(getArticles);
   auth.loginBtn();
 };
 
