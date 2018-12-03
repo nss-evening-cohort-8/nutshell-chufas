faedcad616ec7d48c4c8367e986614909dbf47c2
Maggie Leavell <mjleavell@gmail.com>
2018-12-02 21:52:47 -0600

    edit delete only appear for current uid

--- src/components/Messages/messages.js
+++ src/components/Messages/messages.js
@@ -1,6 +1,6 @@
 /* eslint-disable max-len */
 import $ from 'jquery';
-// import authHelpers from '../../helpers/authHelpers';
+import authHelpers from '../../helpers/authHelpers';
 import messageData from '../../helpers/Data/messageData';
 import messageHelpers from '../../helpers/messageHelpers';
 import './messages.scss';
@@ -12,38 +12,54 @@ import './messages.scss';
 // };
 
 const printAllMessages = (messagesArray) => {
+  const currentUid = authHelpers.getCurrentUid();
   let domString = '';
   domString += '<input type="text" class="form-control" id="msg-input" placeholder="Enter your message then press enter">';
   if (messagesArray.length > 20) {
     messagesArray.shift(messagesArray.length - 20, messagesArray.length);
   }
   messagesArray.forEach((message) => {
-    domString += `
-    <div class="msg-row row m-1" id="${message.id}">
-      <div class="col-md-2 p-0">
-        <p class="text-left">${messageHelpers.getCurrentTime(message.timestamp)}</p>
+    if (message.userUid === currentUid) {
+      domString += `
+      <div class="msg-row row m-1" id="${message.id}">
+        <div class="col-md-2 p-0">
+          <p class="text-left">${messageHelpers.convertTimestamp(message.timestamp)}</p>
+        </div>
+        <div class="col-md-2">
+          <p class="text-left"><strong>${messageHelpers.getCurrentUserName()}</strong></p>
+        </div>
+        <div class="col-md-6">
+          <p class="text-left">${message.message}</p>
+        </div>
+        <div class="col-md-2 row justify-content-end align-items-center">
+          <button type="button" class="edit-btn msg-btn btn btn-success btn-sm" data-edit-btn-id=${message.id}>
+            <i class="far fa-edit"></i>
+          </button>
+          <button type="button" class="del-btn msg-btn btn btn-danger btn-sm" data-delete-btn-id=${message.id}>
+            <i class="far fa-trash-alt"></i></button>
+        </div>
       </div>
-      <div class="col-md-2">
-        <p class="text-left"><strong>${messageHelpers.getCurrentUserName()}</strong></p>
+      <hr>`;
+    } else {
+      domString += `
+      <div class="msg-row row m-1" id="${message.id}">
+        <div class="col-md-2 p-0">
+          <p class="text-left">${messageHelpers.convertTimestamp(message.timestamp)}</p>
+        </div>
+        <div class="col-md-2">
+          <p class="text-left"><strong>${messageHelpers.getCurrentUserName()}</strong></p>
+        </div>
+        <div class="col-md-6">
+          <p class="text-left">${message.message}</p>
+        </div>
       </div>
-      <div class="col-md-6">
-        <p class="text-left">${message.message}</p>
-      </div>
-      <div class="col-md-2 row justify-content-end align-items-center">
-        <button type="button" class="edit-btn msg-btn btn btn-success btn-sm" data-edit-btn-id=${message.id}>
-          <i class="far fa-edit"></i>
-        </button>
-        <button type="button" class="del-btn msg-btn btn btn-danger btn-sm" data-delete-btn-id=${message.id}>
-          <i class="far fa-trash-alt"></i></button>
-      </div>
-    </div>
-    <hr>`;
+      <hr>`;
+    }
   });
   $('#message-board').html(domString);
 };
 
 const getAllMessages = () => {
-  // const uid = authHelpers.getCurrentUid();
   messageData.getAllMessages().then((messagesArray) => {
     printAllMessages(messagesArray);
   });
--- src/helpers/messageHelpers.js
+++ src/helpers/messageHelpers.js
@@ -1,12 +1,29 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
-const getCurrentTime = () => {
+const getCurrentTimestamp = () => {
   const dt = new Date();
   const localDate = dt.toLocaleString();
   return localDate;
 };
 
+const convertTimestamp = (timestamp) => {
+  const dt = new Date(timestamp);
+  const convertedTime = dt.toLocaleString();
+  return convertedTime;
+};
+
 const getCurrentUserName = () => firebase.auth().currentUser.displayName;
 
-export default { getCurrentTime, getCurrentUserName };
+// const getCurrentUserName = () => {
+//   admin.auth().getUser(uid)
+//     .then((userRecord) => {
+//     // See the UserRecord reference doc for the contents of userRecord.
+//       console.log('Successfully fetched user data:', userRecord.toJSON());
+//     })
+//     .catch((error) => {
+//       console.log('Error fetching user data:', error);
+//     });
+// };
+
+export default { getCurrentTimestamp, getCurrentUserName, convertTimestamp };
