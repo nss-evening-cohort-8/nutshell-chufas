d939873484c3018e0f6b8b7418afa12bf729aacc
Maggie Leavell <mjleavell@gmail.com>
2018-12-09 12:16:20 -0600

    WIP

--- src/components/Users/users.js
+++ src/components/Users/users.js
@@ -1,31 +1,15 @@
-// import $ from 'jquery';
 import userData from '../../helpers/Data/userData';
 import authHelpers from '../../helpers/authHelpers';
-
-// const getUserName = (getCurrentUid) => {
-//   // const currentUid = authHelpers.getCurrentUid();
-//   const userObject = {
-//     userUid: getCurrentUid(),
-//     currentUsername: authHelpers.getCurrentUserName(),
-//   };
-//   userData.createNewUser(userObject).then((data) => {
-//     console.log(data.data);
-//     console.log(userObject);
-//   });
-// };
 
 const createNewUser = (currentUid) => {
   const uid = currentUid();
   userData.getSingleUser(uid).then((result) => {
-    console.log(result.data);
     const userObject = Object.keys(result.data).length;
-    console.log(userObject);
     if (userObject === 0) {
       const newUserObject = {
         userUid: uid,
         currentUsername: authHelpers.getCurrentUserName(),
       };
-      console.log(newUserObject);
       userData.createSingleUser(newUserObject);
     }
   });
--- src/helpers/Data/userData.js
+++ src/helpers/Data/userData.js
@@ -3,8 +3,26 @@ import apiKeys from '../../../db/apiKeys.json';
 
 const baseUrl = apiKeys.firebaseKeys.databaseURL;
 
+const getCurrentUserByUid = uid => new Promise((resolve, reject) => {
+  axios.get(`${baseUrl}/users.json?orderBy="userUid"&equalTo="${uid}"`)
+    .then((result) => {
+      const usersObject = result.data;
+      const usersArray = [];
+      if (usersArray !== null) {
+        Object.keys(usersObject).forEach((userId) => {
+          usersObject[userId].id = userId;
+          usersArray.push(usersObject[userId]);
+        });
+      }
+      resolve(usersArray);
+    })
+    .catch((err) => {
+      reject(err);
+    });
+});
+
 const getSingleUser = uid => axios.get(`${baseUrl}/users.json?orderBy="userUid"&equalTo="${uid}"`);
 
 const createSingleUser = userObject => axios.post(`${baseUrl}/users.json`, JSON.stringify(userObject));
 
-export default { getSingleUser, createSingleUser };
+export default { getCurrentUserByUid, getSingleUser, createSingleUser };
