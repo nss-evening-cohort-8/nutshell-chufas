d43dca69b074432e2d1b309c0ae852bdf8604948
feven mulugeta <fevenmulugeta@fevens-MacBook-Pro.local>
2018-12-09 18:55:14 -0600

    conflictttt resolved

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -29,7 +29,7 @@ const navbarEvents = () => {
 
 const createNavbar = () => {
   const domString = `
-  <nav class="navbar navbar-expand-lg" id="main-nav">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="main-nav">
   <a class="navbar-brand text-light" style="font-size: 30px">Chufas</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
